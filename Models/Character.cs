cf2b3db635f7f2c31be1cd0895a96c5c0e66970b
Vaibhav Kumar Ojha <vaibhav.ko@yahoo.com>
2022-08-15 11:44:10 +0530

    Proper Service Response with Generics
    Adding serviceResponse to access it with serviceResponse class

--- Models/Character.cs
+++ Models/Character.cs
@@ -14,5 +14,10 @@ namespace dotnet_rpg.Models
         public int Defense { get; set; } = 10;
         public int Intelligence { get; set; } = 10;
         public RpgClass Class { get; set; } = RpgClass.Knight;
+
+        public static implicit operator List<object>(Character? v)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Services/CharacterService/ICharacterService.cs
+++ Services/CharacterService/ICharacterService.cs
@@ -8,8 +8,8 @@ namespace dotnet_rpg.Services.CharacterService
     public interface ICharacterService
     {
         // adding Task type to our return type for asynchronous calls
-        Task<List<Character>> GetAllCharacters();
-        Task<Character> GetCharacterById(int id);
-        Task<List<Character>> AddCharacter(Character newCharacter);
+        Task<ServiceResponse<List<Character>>> GetAllCharacters();
+        Task<ServiceResponse<Character>> GetCharacterById(int id);
+        Task<ServiceResponse<List<Character>>> AddCharacter(Character newCharacter);
     }
 }
